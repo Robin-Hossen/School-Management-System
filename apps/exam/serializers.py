f43bec1e858cab73870fa5d8b893febdfb6e39a2
Robin Hossen <hossenrobin215@gmail.com>
2026-08-11 15:24:31 +0600

    exam APP updated

--- apps/exam/serializers.py
+++ apps/exam/serializers.py
@@ -1,18 +1,50 @@
 from rest_framework import serializers
-from .models import Exam, ExamSubject,Result
+
+from .models import Exam, ExamSubject, Result
 
 
 class ExamSerializer(serializers.ModelSerializer):
+
+    academic_session_name = serializers.CharField(
+        source="academic_session.name",
+        read_only=True
+    )
+
+    class_name_name = serializers.CharField(
+        source="class_name.name",
+        read_only=True
+    )
+
     class Meta:
         model = Exam
-        fields = "__all__"
+        fields = [
+            "id",
+            "name",
+            "exam_type",
+
+            "academic_session",
+            "academic_session_name",
+
+            "class_name",
+            "class_name_name",
+
+            "start_date",
+            "end_date",
+
+            "created_at",
+            "updated_at",
+        ]
+
 
 class ExamSubjectSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = ExamSubject
-        fields = "__all__"      
+        fields = "__all__"
+
 
 class ResultSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Result
         fields = "__all__"
