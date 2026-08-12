14ae0fd3f76a5ace1c84057788d902f06d70b6e6
Robin Hossen <hossenrobin215@gmail.com>
2026-08-12 17:39:19 +0600

    student model.py updated

--- apps/students/models.py
+++ apps/students/models.py
@@ -15,6 +15,7 @@ class Student(models.Model):
     gender=models.CharField(max_length=10,blank=True)
     address=models.TextField(blank=True)
     guardian_name=models.CharField(max_length=100,blank=True)
+    guardian_email = models.EmailField(blank=True,null=True)
     guardian_phone=models.CharField(max_length=15,blank=True)
     admission_date=models.DateField(auto_now_add=True)
     created_at=models.DateTimeField(auto_now_add=True)
